1ad7994e9691f5f4cb5c30fb89b5bc85cde531dd
Greate-Seagull <h0274.hmt@gmail.com>
2025-11-18 17:02:35 +0700

    feat: update pelican shooting

--- scripts/enemies/minions/shootable/pelican/pelican.gd
+++ scripts/enemies/minions/shootable/pelican/pelican.gd
@@ -9,6 +9,9 @@ extends ShootableEnemy
 
 @export var bullet_impulse: Vector2 = Vector2(75, -200)
 
+@export var left_bound: float = -100
+@export var right_bound: float = 100
+
 func _ready() -> void:
 	super._ready()
 	player_detection_raycast.target_position.y = sight
@@ -23,3 +26,18 @@ func fire():
 func start_shoot() -> void:
 	super.start_shoot()
 	_movement_speed = 0
+	aim()
+
+func try_patrol_turn(_delta: float) -> bool:
+	if is_touch_wall() or has_reached_bounds() or want_to_turn():
+		turn()
+		return true
+	return false
+
+func has_reached_bounds() -> bool:
+	var has_reached_left = (position.x <= left_bound) and (direction == -1)
+	var has_reached_right = (position.x >= right_bound) and (direction == 1)
+	return has_reached_left or has_reached_right
+
+func aim() -> void:
+	change_direction(found_player.direction)
