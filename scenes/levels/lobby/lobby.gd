3a3ae6b841ef33ad9b6e6f6ddaaf640fe716b37b
Lê Bá Đoàn <doanryder2005@gmail.com>
2025-11-22 12:57:35 +0700

    Thay đổi level flow, fix save/load, load level, và tạo lobby

---
+++ scenes/levels/lobby/lobby.gd
@@ -0,0 +1,7 @@
+extends Node2D
+
+@onready var tutorial_finish_marker: Marker2D = $World/TutorialFinishedMarker
+
+func _ready() -> void:
+	if GameManager.is_tutorial_finished:
+		GameManager.player.global_position = tutorial_finish_marker.global_position
--- scripts/object/door/win_door.gd
+++ scripts/object/door/win_door.gd
@@ -10,16 +10,7 @@ func _ready() -> void:
 	animated_sprite.play("idle")
 
 func _process(delta):
-	if player_in_area and not win_ui_shown:
-		
-		if Input.is_action_just_pressed("interact"):
-			win_ui_shown = true 
-			var scene = load("res://scenes/ui/WinUI.tscn").instantiate()
-			get_tree().root.add_child(scene)
-			var nine_patch = scene.get_node("NinePatchRect")
-			if nine_patch:
-				var viewport_size = get_viewport().get_visible_rect().size
-				nine_patch.position = (viewport_size - nine_patch.size) / 2
+	pass
 
 func load_next_stage():
 	var current_stage_path = get_tree().current_scene.scene_file_path
@@ -47,7 +38,16 @@ func move_player_to_door():
 	print("Đã di chuyển người chơi đến cửa: ", target_door)
 
 func _on_interactive_area_2d_interacted() -> void:
-	load_next_stage()
+	GameManager.unlock_level()
+	if player_in_area and not win_ui_shown:
+		win_ui_shown = true 
+		var scene = load("res://scenes/ui/WinUI.tscn").instantiate()
+		get_tree().root.add_child(scene)
+		var nine_patch = scene.get_node("NinePatchRect")
+		if nine_patch:
+			var viewport_size = get_viewport().get_visible_rect().size
+			nine_patch.position = (viewport_size - nine_patch.size) / 2
+	print("is it unlocking a new level?")
 
 func _on_detection_area_2d_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
