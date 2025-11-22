c58d3726347cee9b091ac43fbad393774c51253e
Phạm Triều Dương <phamduong1132005@gmail.com>
2025-11-22 12:02:33 +0700

    add material selection flow

--- scripts/crafting/weapon_material_data.gd
+++ scripts/crafting/weapon_material_data.gd
@@ -4,3 +4,4 @@ class_name WeaponMaterialData
 @export var id: String
 @export var display_name: String
 @export var color: Color = Color.WHITE
+@export var icon: Texture2D
--- scripts/npc/anvil.gd
+++ scripts/npc/anvil.gd
@@ -1,11 +1,16 @@
 extends InteractiveArea2D
 
 @export var minigame_ui: Control
+@export var material_panel: Control
+
 func _ready() -> void:
 	interacted.connect(_on_interacted)
 	super._ready()
 	
 func _on_interacted():
 	print("interact with anvil")
-	if minigame_ui:
-		minigame_ui.open()
+	material_panel.open()
+	material_panel.material_selected.connect(_start_minigame)
+		
+func _start_minigame(mat_id):
+	minigame_ui.open(mat_id)
