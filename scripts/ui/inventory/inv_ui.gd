27026e21608024237b488c3d93c1e84c5d137073
Phạm Triều Dương <phamduong1132005@gmail.com>
2025-12-06 16:38:39 +0700

    load weapon into inventory

--- scripts/ui/inventory/inv_ui.gd
+++ scripts/ui/inventory/inv_ui.gd
@@ -2,7 +2,9 @@ extends Control
 class_name InvUI
 
 @onready var slots := $Panel/MarginContainer/GridContainer.get_children()
-
+func _ready() -> void:
+	update_inventory_ui()
+	
 func update_inventory_ui():
 	var archive = GameManager.player.inventory.item_archive
 
@@ -26,15 +28,36 @@ func update_inventory_ui():
 			continue
 
 		var count :int = item_detail_list.size()
-		var icon := load_icon(item_type)
+		var icon := load_icon(item_type, item_detail_list)
 
 		slots[i].set_item(icon, item_type, item_detail_list, count)
 
 
-func load_icon(item_type: String) -> Texture2D:
-	var path := "res://ui/icons/%s.png" % item_type
-	if ResourceLoader.exists(path):
-		return load(path)
-	else:
-		print("⚠️ Không tìm thấy icon cho:", item_type)
+func load_icon(item_type: String, item_detail_list := []) -> Texture2D:
+
+	if item_type.begins_with("weapon_"):
+		if item_detail_list.size() > 0 and item_detail_list[0] is WeaponData:
+			var weapon: WeaponData = item_detail_list[0]
+			var icon_path := weapon.png_path
+
+			if icon_path != "" and FileAccess.file_exists(icon_path):
+				var img := Image.load_from_file(icon_path)
+				if img:
+					return ImageTexture.create_from_image(img)
+
+				printerr("⚠ Failed to load image from file: ", icon_path)
+				return null
+
+			printerr("⚠ Weapon icon not found at: ", icon_path)
+			return null
+
+		printerr("⚠ Invalid weapon data for: ", item_type)
 		return null
+
+	var default_path := "res://ui/icons/%s.png" % item_type
+
+	if ResourceLoader.exists(default_path):
+		return load(default_path)
+
+	printerr("⚠ Default icon not found for item_type: ", item_type)
+	return null
