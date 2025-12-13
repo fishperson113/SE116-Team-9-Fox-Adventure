81121f3353a7367a9a640f8ecfaf36db8bf6fb89
Lê Bá Đoàn <doanryder2005@gmail.com>
2025-12-13 20:31:01 +0700

    Thêm coin, thay đổi độ sáng background cho lobby

---
+++ scripts/collectibles/items/collectibles/collectible_coin.gd
@@ -0,0 +1,13 @@
+extends CollectibleOre
+class_name CollectibleCoin
+
+var coin_amount: int = 1
+
+func _ready() -> void:
+	animated_sprite.play("default")
+	pass
+
+func _on_interaction_available() -> void:
+	GameManager.add_coins(coin_amount)
+	queue_free()
+	pass
